d8c50264b93c84d200cb31133df3f9ebb3808927
wendigo453453 <77675933+wendigo453453@users.noreply.github.com>
2023-04-06 15:41:58 +0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class Database:
+    def __init__(self, dbu_file):
+        self.connection = sqlite3.connect(dbu_file)
+        self.cursor = self.connection.cursor()
+
+    def user_exists(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
+            return bool(len(result))
+
+    def add_user(self, user_id):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO `users` (`user_id`) VALUES (?)", (user_id,))
+
+    def set_active(self, user_id, active):
+        with self.connection:
+            return self.cursor.execute("UPDATE `users` SET `active = ? WHERE `user_id` = ?", (active, user_id,))
+
+    def get_users(self):
+        with self.connection:
+            return self.cursor.execute("SELECT `user_id`, `active` FROM `users`").fetchall()
